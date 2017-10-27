6b2d2677108dbf60f77d4a3f4c46137f52e7f9a1
David <David@David-PC>
2017-10-27 16:53:07 +0200

    Musician entity, musician DAo

---
+++ music-library-persistence/src/main/java/cz/fi/muni/pa165/dao/MusicianDaoImpl.java
@@ -0,0 +1,51 @@
+package cz.fi.muni.pa165.dao;
+
+import cz.fi.muni.pa165.entity.Musician;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author David Koncak
+ */
+@Repository
+public class MusicianDaoImpl implements MusicianDao {
+    
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public void create(Musician m) {
+        em.persist(m);
+    }
+
+    @Override
+    public void remove(Musician m) throws IllegalArgumentException {
+        em.remove(findById(m.getId()));
+    }
+
+    @Override
+    public void update(Musician m) {
+        em.merge(m);
+    }
+
+    @Override
+    public Musician findById(Long id) {
+        return em.find(Musician.class, id);
+    }
+
+    @Override
+    public List<Musician> findByName(String namePattern) {
+        return em.createQuery("SELECT m FROM Musician m WHERE m.name like :name ",
+				Musician.class).setParameter("name", "%"+namePattern+"%").getResultList();
+    }
+
+    @Override
+    public List<Musician> findAll() {
+        return em.createQuery("select m from Musicians m", Musician.class)
+				.getResultList();
+    }
+    
+}
