866f2b4065f6203cab648601453abab581a37579
Jan-Sebastian Fabík <honza@fabik.org>
2017-10-29 02:14:20 +0100

    Normalized whitespace

--- music-library-persistence/src/main/java/cz/fi/muni/pa165/musiclibrary/dao/MusicianDao.java
+++ music-library-persistence/src/main/java/cz/fi/muni/pa165/musiclibrary/dao/MusicianDao.java
@@ -8,11 +8,11 @@ import java.util.List;
  * @author David Koncak
  */
 public interface MusicianDao {
-    public void create(Musician m);
-    public void remove(Musician m) throws IllegalArgumentException;
-    public void update(Musician m);
-    public Musician findById(Long id);
-    public List<Musician> findByName(String namePattern);
-    public List<Musician> findAll();
-    
+	public void create(Musician m);
+	public void remove(Musician m) throws IllegalArgumentException;
+	public void update(Musician m);
+	public Musician findById(Long id);
+	public List<Musician> findByName(String namePattern);
+	public List<Musician> findAll();
+
 }
