a3178f95df5d1e459f83429f74d50e97a2c91980
Jan-Sebastian Fabik <jansfabik@gmail.com>
2017-11-23 15:03:03 +0100

    Added API module

---
+++ music-library-api/src/main/java/cz/fi/muni/pa165/musiclibrary/exceptions/MusicLibraryServiceException.java
@@ -0,0 +1,26 @@
+package cz.fi.muni.pa165.musiclibrary.exceptions;
+
+public class MusicLibraryServiceException extends RuntimeException {
+
+    public MusicLibraryServiceException() {
+        super();
+    }
+
+    public MusicLibraryServiceException(String message, Throwable cause,
+                                        boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public MusicLibraryServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MusicLibraryServiceException(String message) {
+        super(message);
+    }
+
+    public MusicLibraryServiceException(Throwable cause) {
+        super(cause);
+    }
+
+}
