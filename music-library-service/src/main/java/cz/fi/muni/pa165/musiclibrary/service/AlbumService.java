9d11bb0c755a98ee7de6b0feb49e73f2b58ab44d
root <root@IVUSH.localdomain>
2017-11-26 19:41:26 +0100

    adding AlbumService interface

---
+++ music-library-service/src/main/java/cz/fi/muni/pa165/musiclibrary/service/AlbumService.java
@@ -0,0 +1,29 @@
+package cz.fi.muni.pa165.musiclibrary.service;
+
+import cz.fi.muni.pa165.musiclibrary.entity.Album;
+import cz.fi.muni.pa165.musiclibrary.entity.Genre;
+import cz.fi.muni.pa165.musiclibrary.entity.Musician;
+
+import java.util.List;
+
+/**
+ *
+ * @author Iva Liberova
+ */
+public interface AlbumService {
+    void create(Album album);
+
+    void update(Album album) throws IllegalArgumentException;
+
+    void remove(Album album) throws IllegalArgumentException;
+
+    Album findById(Long id);
+
+    List<Album> findByMusician(Musician musician);
+
+    List<Album> findByGenre(Genre genre);
+
+    List<Album> findByTitle(String titlePattern);
+
+    List<Album> findAll();
+}
