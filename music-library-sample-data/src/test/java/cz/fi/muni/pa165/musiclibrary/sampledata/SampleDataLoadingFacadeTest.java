99f18e93958724c334a1bd7dcd1711b43be7df86
Jan-Sebastian Fabík <honza@fabik.org>
2017-12-17 21:33:19 +0100

    Added test for sample data loading facade

--- music-library-sample-data/src/test/java/cz/fi/muni/pa165/musiclibrary/sampledata/SampleDataLoadingFacadeTest.java
+++ music-library-sample-data/src/test/java/cz/fi/muni/pa165/musiclibrary/sampledata/SampleDataLoadingFacadeTest.java
@@ -1,5 +1,6 @@
 package cz.fi.muni.pa165.musiclibrary.sampledata;
 
+import cz.fi.muni.pa165.musiclibrary.entity.Musician;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.transaction.annotation.Transactional;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import javax.persistence.EntityManager;
@@ -24,13 +26,20 @@ public class SampleDataLoadingFacadeTest extends AbstractTestNGSpringContextTest
 	final static Logger log = LoggerFactory.getLogger(SampleDataLoadingFacadeTest.class);
 
 	@Autowired
-	public SampleDataLoadingFacade sampleDataLoadingFacade;
+	private SampleDataLoadingFacade sampleDataLoadingFacade;
 
 	@PersistenceContext
 	private EntityManager em;
 
 	@Test
-	public void createSampleData() {
-		// TODO
+	public void loadData() {
+		sampleDataLoadingFacade.loadData();
+
+		Musician musician = em.createQuery("SELECT m FROM Musician m WHERE m.name = :name", Musician.class)
+			.setParameter("name", "Ed Sheeran")
+			.getSingleResult();
+
+		Assert.assertNotNull(musician);
+		Assert.assertEquals("Ed Sheeran", musician.getName());
 	}
 }
