954e53c2ade467e72202217c6241ba6ccdfd6b21
Jan-Sebastian Fabík <honza@fabik.org>
2017-12-18 19:10:25 +0100

    Replaced Inject annotation with Autowired

--- music-library-web/src/main/java/cz/fi/muni/pa165/musiclibrary/web/controllers/LoginController.java
+++ music-library-web/src/main/java/cz/fi/muni/pa165/musiclibrary/web/controllers/LoginController.java
@@ -7,6 +7,7 @@ package cz.fi.muni.pa165.musiclibrary.web.controllers;
 
 import cz.fi.muni.pa165.musiclibrary.dto.ApplicationUserDTO;
 import cz.fi.muni.pa165.musiclibrary.facade.ApplicationUserFacade;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -17,17 +18,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-
 
 @Controller
 @RequestMapping("/")
 public class LoginController {
 
-	@Inject
-	ApplicationUserFacade userFacade;
+	@Autowired
+	private ApplicationUserFacade applicationUserFacade;
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login(Model model, HttpServletRequest request) {
@@ -53,10 +52,9 @@ public class LoginController {
 			return "/login";
 		}
 
-		ApplicationUserDTO found = userFacade.findByEmail(form.getEmail());
+		ApplicationUserDTO found = applicationUserFacade.findByEmail(form.getEmail());
 
-		if (found == null) // missing facade check
-		{
+		if (found == null) { // missing facade check
 			redirectAttributes.addFlashAttribute("alert_warning", "Login with username " + form.getEmail() + " has failed.");
 			return "redirect:" + uriBuilder.path("/login").toUriString();
 		}
